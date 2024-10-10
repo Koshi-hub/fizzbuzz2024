0a99f9ec1552d60d7ced8c8b70c7ec8afaee4938
Koshi-hub <ookosi3390@gmail.com>
2024-10-10 17:45:53 +0900

    Update kadai2.py

--- kadai2.py
+++ kadai2.py
@@ -3,7 +3,7 @@
 # ユーザーから最大数を入力
 ookoshi = int(input("数える最大数を入力してください: "))
 
-# 1からmax_numまで数えてFizzBuzzを実行
+# 1からookoshiまで数えてFizzBuzzを実行
 for kousei in range(1, ookoshi + 1):
     if kousei % 15 == 0:
         print("Fizz Buzz")
